905ece69b1784c6d4a5732e5ae588f436d2b1d8a
Nikita Gnotov <nikita@gnotov.com>
2015-06-25 23:50:49 +0300

    Add floating link to contact form

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -7,13 +7,15 @@ $(document).ready(function() {
 	scroll_top_duration = 700,
 	//grab the "back to top" link
 	$back_to_top = $('.cd-top');
+	$to_contacts = $('.cd-top-envelope');
 
 	//hide or show the "back to top" link
 	$(window).scroll(function(){
 		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		if( $(this).scrollTop() > offset_opacity ) {
-			$back_to_top.addClass('cd-fade-out');
-		}
+		( $(this).scrollTop() > offset ) ? $to_contacts.addClass('cd-is-visible') : $to_contacts.removeClass('cd-is-visible cd-fade-out');
+		// if( $(this).scrollTop() > offset_opacity ) {
+		// 	$back_to_top.addClass('cd-fade-out');
+		// }
 	});
 
 	//smooth scroll to top
@@ -22,6 +24,15 @@ $(document).ready(function() {
 		$('body,html').animate({
 			scrollTop: 0 ,
 		 	}, scroll_top_duration
+		);
+	});
+
+	//smooth scroll to contacts
+	$to_contacts.on('click', function(event){
+		event.preventDefault();
+		$('html, body').animate({
+      scrollTop: $("#section-contact").offset().top
+    	}, scroll_top_duration
 		);
 	});
 });
