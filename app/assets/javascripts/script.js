9ce1ab0a3c36162fb94c267a07dccd4e53bfa424
Nikita Gnotov <nikita@gnotov.com>
2015-06-26 02:25:37 +0300

    add favicon

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   // browser window scroll (in pixels) after which the "back to top" link is shown
 	var offset = 300,
+	contacts_offset = 0,
 	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
 	offset_opacity = 1200,
 	//duration of the top scrolling animation (in ms)
@@ -12,7 +13,7 @@ $(document).ready(function() {
 	//hide or show the "back to top" link
 	$(window).scroll(function(){
 		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
-		( $(this).scrollTop() > offset ) ? $to_contacts.addClass('cd-is-visible') : $to_contacts.removeClass('cd-is-visible cd-fade-out');
+		( $(this).scrollTop() > contacts_offset ) ? $to_contacts.addClass('cd-is-visible') : $to_contacts.removeClass('cd-is-visible cd-fade-out');
 		// if( $(this).scrollTop() > offset_opacity ) {
 		// 	$back_to_top.addClass('cd-fade-out');
 		// }
